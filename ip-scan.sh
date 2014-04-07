64b694a6347e8cd1d8578647688b063febb6f281
prolin99 <prolin99@gmail.com>
2014-04-07 15:20:09 +0800

    掃描範例檔

---
+++ ip-scan.sh
@@ -0,0 +1,17 @@
+	#!/bin/bash
+ 
+	PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+	export PATH
+	
+	#nmap 掃描網載，產生記錄文字檔，要放在網頁可以存區處 (以下為範例，要修改網域，及要存放的位置)，
+	#在偏好設定，nmap掃描結果檔   http://網址/nmap.txt
+	/usr/bin/nmap -sP 120.116.24.0/24 > /var/www/html/nmap.txt
+	
+	#取得 ipv6 資訊
+	#在偏好設定，arp 掃描結果檔   http://網址/arp.txt
+	ping6 -I eth0 -c3 ff02::1 > /dev/null
+	/sbin/ip neigh show > /var/www/html/arp.txt
+	
+	
+	#其中代碼為偏好設定--定時連線的代號 (現預設 info_whats )
+	wget -O /dev/null http://你的網址/modules/info_whats/comp.php?do=info_whats   2> /dev/null
