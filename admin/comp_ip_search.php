42941c4c1c033a2e9402e377dfc1bb0831f06287
prolin99 <prolin99@gmail.com>
2015-12-22 15:56:09 +0800

    檔頭修正

--- admin/comp_ip_search.php
+++ admin/comp_ip_search.php
@@ -1,15 +1,9 @@
 <?php
-//$Id: comp.php,v 1.1.2.6 2005/11/01 15:33:15 brucelyc Exp $
-include "config.php";
-//匯入資料
 
- 
 if ($_GET['sip'] ) {
 	echo '查詢 ip : ' . $_GET['sip']   ."\n" ;
- 
+
 	$ip_list =  shell_exec("nmap  --script smb-os-discovery -p 445 -Pn  " .  $_GET['sip']  ) ;
 	echo $ip_list  ;
- 
- 
- 
+
 }
