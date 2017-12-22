de8bc73312d19d435d3cfa4dab9e32335f1e207a
prolin99 <prolin99@gmail.com>
2017-12-22 08:39:07 +0800

    連線資料也要刪除

--- admin/comp_del.php
+++ admin/comp_del.php
@@ -1,14 +1,23 @@
-<?php 
+<?php
  include_once "header.php";
-if ($_GET['del_id'] ) {
-	$id_array = preg_split('/_/',$_GET['del_id'] ) ;
-	$id = $id_array[1]  ;
-	if ( $id >0 ) {
-  		$sql = " delete from " . $xoopsDB->prefix("mac_info") .  " where id='$id' " ;
- 		$result = $xoopsDB->queryF($sql) or die($sql."<br>". $xoopsDB->error()); 					
- 
-     		echo "delete $id  " ;
-     	}else {
-     		echo "delete error  {$_GET['del_id'] }  " ;
-     	}
-}     	
+if ($_GET['del_id']) {
+    $id_array = preg_split('/_/', $_GET['del_id']) ;
+    $id = $id_array[1]  ;
+    if ($id >0) {
+        //機器資料
+        $sql = " delete from " . $xoopsDB->prefix("mac_info") .  " where id='$id' " ;
+        $result = $xoopsDB->queryF($sql) or die($sql."<br>". $xoopsDB->error());
+
+        //連線資料
+        $sql = " delete from " . $xoopsDB->prefix("mac_online") .  " where id='$id' " ;
+        $result = $xoopsDB->queryF($sql) or die($sql."<br>". $xoopsDB->error());
+
+        //上傳資料
+        $sql = " delete from " . $xoopsDB->prefix("mac_up_sysinfo") .  " where id='$id' " ;
+        $result = $xoopsDB->queryF($sql) or die($sql."<br>". $xoopsDB->error());
+
+        echo "delete $id  " ;
+    } else {
+        echo "delete error  {$_GET['del_id'] }  " ;
+    }
+}
